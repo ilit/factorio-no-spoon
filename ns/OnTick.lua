868467f46c0ccb984058905b10dbf31c72b7bc7e
Your Name <you@example.com>
2022-09-18 01:42:58 +0300

    ref comments

--- ns/OnTick.lua
+++ ns/OnTick.lua
@@ -1,13 +1,21 @@
-local updateSporeInjectors = require "UpdateSporeInjectors"
+local updateSporeInjections = require "UpdateSporeInjections"
 
 sporeInjections = ttv.new()
 
 return function(tick)
     if tick % 60 == 0 then
-        sporeInjections = updateSporeInjectors(
+        -- // Dont bother with steps fixed in time. Make a step when performance allows.
+        -- // Dont exec steps faster than minimum ticks. Maintain startingTickOfTheLastStep.
+
+        sporeInjections = updateSporeInjections(
                 game.print,
                 tick,
                 game.surfaces["nauvis"],
                 sporeInjections)
+
+        -- Eval ContaminatedField by SporeInjections blurring\math. Each source is a layer to add?
+        -- Eval ContaminatedTiles
+        -- Apply noise distortion
+        -- Try to update a tile. --TODO Skip update if it is already of correct name.
     end
 end
