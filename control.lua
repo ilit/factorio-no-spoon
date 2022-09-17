1bcb42a2bff135343da576c3b0ae2a4720406890
Ilja Litvinov <ilitvinov@masterhost.ru>
2022-09-17 09:28:52 +0300

    updateSporeInjectors

--- control.lua
+++ control.lua
@@ -1,23 +1,16 @@
-local ontick = require "ns/OnTick"
+--- Global Libs ---
 ttv = require "ns/TileToValues"
+---
 
-sporeInjections = {}
+local ontick = require "ns/OnTick"
 
 local registerEvents = function()
     script.on_event(defines.events.on_tick, function(e)
-        if e.tick % 60 == 0 then
-            sporeInjections = ontick(
-                    game.print,
-                    e.tick,
-                    game.surfaces["nauvis"],
-                    sporeInjections)
-        end
+        ontick(e.tick)
     end)
 end
 
 local init = function()
-    sporeInjections = ttv.new()
-
     registerEvents()
 end
 
--- ns/OnTick.lua
+++ ns/OnTick.lua
@@ -1,31 +1,13 @@
-local evalSporeInjectors = require "ns/SporeInjectors"
-local evalEntitiesToTiles = require "ns/EntitiesToTiles"
-local mergeTileToVals = require "ns/MergeTileToVals"
-local appendsValuesToTiles = require "AppendValuesToTiles"
+local updateSporeInjectors = require "UpdateSporeInjectors"
 
-return function(print, tick, surface, prevSporeInjections)
-    print("tick: "..tick)
+sporeInjections = ttv.new()
 
-    -- // Dont bother with steps fixed in time. Make a step when performance allows.
-    -- // Dont exec steps faster than minimum ticks. Maintain startingTickOfTheLastStep.
-
-    --- ### SporeInjections gather data ###
-    local sporeInjectors = evalSporeInjectors(surface)
-    local sporeInjectorTiles = evalEntitiesToTiles(sporeInjectors)
-    local newInjectionValues = appendsValuesToTiles(sporeInjectorTiles)
-
-    --- ### Append to SporeInjectionsStorage
-    -- TODO fix ttv1 == nil
-    local updatedSporeInjections = mergeTileToVals(prevSporeInjections, newInjectionValues)
-
-    local usi = updatedSporeInjections
-    for i=1, usi.len do
-        print(usi.xs[i] .. " " .. usi.ys[i].. " " .. usi.vals[i])
+return function(tick)
+    if tick % 60 == 0 then
+        sporeInjections = updateSporeInjectors(
+                game.print,
+                tick,
+                game.surfaces["nauvis"],
+                sporeInjections)
     end
-    -- Eval ContaminatedField by SporeInjections blurring\math. Each source is a layer to add?
-    -- Eval ContaminatedTiles
-    -- Apply noise distortion
-    -- Try to update a tile. --TODO Skip update if it is already of correct name.
-
-    return updatedSporeInjections
 end
