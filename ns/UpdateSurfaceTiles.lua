bbbf814d55e7f62db378231cc09769e2073542f9
Ilja Litvinov <ilitvinov@masterhost.ru>
2022-09-21 16:57:57 +0300

    more biomes

--- ns/UpdateSurfaceTiles.lua
+++ ns/UpdateSurfaceTiles.lua
@@ -10,10 +10,12 @@ return function(surface, contamTiles)
 
         local name
         local randomBiomeSeedFloat = biomeSeedGenerator(pos)
-        if randomBiomeSeedFloat < 0.5 then
-            name = "vegetation-purple-grass-2"
-        else
-            name = "vegetation-yellow-grass-2"
+        if     randomBiomeSeedFloat < 0.1 then name = "vegetation-mauve-grass-2"
+        elseif randomBiomeSeedFloat < 0.2 then name = "vegetation-red-grass-2"
+        elseif randomBiomeSeedFloat < 0.4 then name = "vegetation-blue-grass-2"
+        elseif randomBiomeSeedFloat < 0.6 then name = "vegetation-yellow-grass-2"
+        elseif randomBiomeSeedFloat < 0.8 then name = "vegetation-violet-grass-2"
+        else name = "vegetation-purple-grass-2"
         end
 
         table.insert(reformattedTiles, { name = name,
