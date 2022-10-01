9004f7a59f0e3d78b50cf82ffa9dd4682b5a0c74
Your Name <you@example.com>
2022-10-02 01:22:26 +0300

    _

--- ns/ContamFields/EvalContaminatedField.lua
+++ ns/ContamFields/EvalContaminatedField.lua
@@ -1,8 +1,8 @@
-return function(xCenter, yCenter, dist)
+return function(xCenter, yCenter, strength)
     local field = Tiles.new()
 
-    if dist > 0 then
-        local radius = dist - 1
+    if strength > 0 then
+        local radius = strength - 1
         for xOff=-radius, radius do
             for yOff=-radius, radius do
                 if xOff*xOff + yOff*yOff <= radius * radius then
--- ns/EvalUpdatedSporeInjections.lua
+++ ns/EvalUpdatedSporeInjections.lua
@@ -4,32 +4,10 @@ local mergeTileToVals = require "ns/UpdateSporeInjectors/MergeTileToVals"
 local appendsValuesToTiles = require "ns/UpdateSporeInjectors/AppendValuesToTiles"
 
 return function(surface, prevSporeInjections)
-    local prof1
-    if prof1 == nil then prof1 = game.create_profiler() end
-
-    prof1.reset()
     local sporeInjectors = evalSporeInjectors(surface)
-    prof1.stop()
-    print("evalSporeInjectors ")
-    print(prof1)
-
-    prof1.reset()
     local sporeInjectorTiles = evalEntitiesToTiles(sporeInjectors)
-    prof1.stop()
-    print("evalEntitiesToTiles ")
-    print(prof1)
-
-    prof1.reset()
     local newInjectionValues = appendsValuesToTiles(sporeInjectorTiles)
-    prof1.stop()
-    print("appendsValuesToTiles ")
-    print(prof1)
-
-    prof1.reset()
     local updatedSporeInjections = mergeTileToVals(prevSporeInjections, newInjectionValues)
-    prof1.stop()
-    print("mergeTileToVals ")
-    print(prof1)
 
     return updatedSporeInjections
 end
